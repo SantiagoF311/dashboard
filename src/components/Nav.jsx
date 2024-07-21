1172abaf6c6885282ad7b05b08bbb24b34b1ba7d
SantiagoF311 <santiagofonsecaf311@gmail.com>
2024-07-21 15:17:49 -0500

    main

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,21 +2,30 @@ import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.svg';
 import DensityMediumIcon from '@mui/icons-material/DensityMedium';
+import { useState } from 'react';
 
 export const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
 
- const title = (
+  const handleMouseEnter = () => {
+    setShowMenu(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowMenu(false);
+  };
+
+  const title = (
     <span className='navButtonTitleContainer'>
       <p className='navButtonText'>Menu</p>
-      <DensityMediumIcon/>
+      <DensityMediumIcon />
     </span>
   );
-
 
   return (
     <Navbar className='bg-body-transparennt customNavbar' expand="md">
       <Container fluid>
-        <Navbar.Brand href="#home" className="logo-brand">
+        <Navbar.Brand as={Link} to={'/'} className="logo-brand">
           <img
             alt=""
             src={logo}
@@ -28,19 +37,30 @@ export const NavBar = () => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="navItemsContainer">
-          <Nav className='navBarButtonContainer'>
+          <Nav
+            className='navBarButtonContainer'
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
             <div className='navBarButton'>
-              <NavDropdown title={title} id="basic-nav-dropdown" className='navButton'>
-              <NavDropdown.Item className='dropdownItem' as={Link} to={'/'}>None</NavDropdown.Item>
+              <NavDropdown
+                title={title}
+                id="basic-nav-dropdown"
+                className={`navButton ${showMenu ? 'show' : ''} menuDropdown`}
+                show={showMenu}
+              >
+                <NavDropdown.Item className='navDropDownItem' as={Link} to={'/'}>
+                  <span className='dropDownItemText'>Inicio</span>
+                </NavDropdown.Item>
               </NavDropdown>
             </div>
           </Nav>
           <Nav className='navLinkContainer'>
-            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link as={Link} to={'/'}>Inicio</Nav.Link>
           </Nav>
           <Nav>
-            <NavDropdown title="Super admin" id="basic-nav-dropdown">
-              <NavDropdown.Item as={Link} to={'/'}>None</NavDropdown.Item>
+            <NavDropdown title="Super admin" id="basic-nav-dropdown" className='userDropDown'>
+              <NavDropdown.Item className='userDropDownItem' as={Link} to={'/'}>None</NavDropdown.Item >
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
